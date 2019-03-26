815cd815a662be26e6769ef7ea547db4a5925189
Nikola Suscevic <nikola.suscevic@maersk.com>
2019-03-26 15:49:49 +0100

    Mongo Reactive Repository - JUnit tests - build artifact

--- src/test/java/com/learnreactivespring/repository/ItemReactiveRepositoryTest.java
+++ src/test/java/com/learnreactivespring/repository/ItemReactiveRepositoryTest.java
@@ -6,8 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @DataMongoTest
 @RunWith(SpringRunner.class)
+@DirtiesContext
 public class ItemReactiveRepositoryTest {
 
     List<Item> itemList = Arrays.asList(new Item(null, "Samsung TV", 400.0),
@@ -56,6 +59,50 @@ public class ItemReactiveRepositoryTest {
         StepVerifier.create(itemReactiveRepository.findByDescription("Bose Headphones").log())
                 .expectSubscription()
                 .expectNextCount(1L)
+                .verifyComplete();
+    }
+
+    @Test
+    public void saveItem() {
+        Item item = new Item(null, "Google Home Mini", 30.00);
+        Mono<Item> savedItem = itemReactiveRepository.save(item);
+        StepVerifier.create(savedItem.log("saveItem: "))
+                .expectSubscription()
+                .expectNextMatches(item1 -> (item1.getId() != null && item1.getDescription().equals("Google Home Mini")))
+                .verifyComplete();
+    }
+
+    @Test
+    public void updateItem() {
+        double newPrice = 520.00;
+        Mono<Item> updatedItem = itemReactiveRepository.findByDescription("LG TV")
+                .map(item -> {
+                    item.setPrice(newPrice);
+                    return item;
+                })
+                .flatMap(item -> {
+                    return itemReactiveRepository.save(item);
+                });
+
+        StepVerifier.create(updatedItem)
+                .expectSubscription()
+                .expectNextMatches(item -> item.getPrice() == newPrice)
+                .verifyComplete();
+    }
+
+    @Test
+    public void deleteItemById() {
+        Mono<Void> deletedItem = itemReactiveRepository.findByDescription("LG TV")
+                .flatMap(item -> itemReactiveRepository.delete(item)
+                );
+
+        StepVerifier.create(deletedItem.log())
+                .expectSubscription()
+                .verifyComplete();
+
+        StepVerifier.create(itemReactiveRepository.findAll().log())
+                .expectSubscription()
+                .expectNextCount(4L)
                 .verifyComplete();
     }
 }
